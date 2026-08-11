bb3b67d3dc6e9aa94a1b41d758a7727ff1295b59
Jesse250535 <jim985211gx@163.com>
2026-08-12 00:45:20 +0800

    Decorate1

---
+++ src/main/java/com/example/decoratebackservice/DecorateBackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.decoratebackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecorateBackServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecorateBackServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/decoratebackservice/DecorateBackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.decoratebackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecorateBackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
